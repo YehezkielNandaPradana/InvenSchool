f9ae5432bcae913c55be380362c2a8a56cc72287
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:54:56 +0700

    add database seeder

--- Api/database/seeders/DatabaseSeeder.php
+++ Api/database/seeders/DatabaseSeeder.php
@@ -2,21 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     public function run(): void
     {
         $this->call([
-            RoleSeeder::class,
-            LokasiSeeder::class,
-            KategoriDanaSeeder::class,
-            KategoriBarangSeeder::class,
             UserSeeder::class,
+            BarangSeeder::class,
         ]);
     }
 }
