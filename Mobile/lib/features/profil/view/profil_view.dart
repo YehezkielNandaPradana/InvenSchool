4601e804ee1876776443276223ce17fadef0539f
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add profil view

---
+++ Mobile/lib/features/profil/view/profil_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/profil/profil_page.dart';
+
+class ProfilView extends StatelessWidget {
+  const ProfilView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ProfilPage();
+  }
+}
