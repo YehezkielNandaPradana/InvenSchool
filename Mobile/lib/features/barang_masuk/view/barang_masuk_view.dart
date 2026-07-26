3eb8cd9830cffdbc3dabe47d531d501d690a2430
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add barang masuk view

---
+++ Mobile/lib/features/barang_masuk/view/barang_masuk_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/barang_masuk/barang_masuk_page.dart';
+
+class BarangMasukView extends StatelessWidget {
+  const BarangMasukView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BarangMasukPage();
+  }
+}
