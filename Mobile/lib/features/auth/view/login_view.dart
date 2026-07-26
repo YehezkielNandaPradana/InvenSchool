1f4174c3db0fb0e2bc06d2a83fbc62456d36cfab
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add login view

---
+++ Mobile/lib/features/auth/view/login_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/auth/login/login_page.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LoginPage();
+  }
+}
