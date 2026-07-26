e7caf0008c70238232dd17e239fd1daf24f0e520
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 10:32:44 +0700

    update baseUrl to 127.0.0.1, allow all cors origins for dev

--- Mobile/lib/app/constants/app_api_config.dart
+++ Mobile/lib/app/constants/app_api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://10.0.2.2:8000/api';
+  static const String baseUrl = 'http://127.0.0.1:8000/api';
   static const int connectTimeout = 15000;
   static const int receiveTimeout = 15000;
   static const String tokenKey = 'token';
