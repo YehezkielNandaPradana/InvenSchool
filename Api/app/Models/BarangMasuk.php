1a3b0ba7757240fcdd2273fb33946167fc7fdf85
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:58 +0700

    add barang masuk model

---
+++ Api/app/Models/BarangMasuk.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BarangMasuk extends Model
+{
+    protected $table = 'barang_masuk';
+
+    protected $fillable = [
+        'barang_id',
+        'jumlah',
+        'tanggal_masuk',
+        'keterangan',
+    ];
+
+    public function barang()
+    {
+        return $this->belongsTo(Barang::class);
+    }
+}
