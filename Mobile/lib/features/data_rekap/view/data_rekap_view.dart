17ad44185951dad14cfc5561ef4259e287d09c07
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add data rekap view

---
+++ Mobile/lib/features/data_rekap/view/data_rekap_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/data_rekap/data_rekap_page.dart';
+
+class DataRekapView extends StatelessWidget {
+  const DataRekapView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DataRekapPage();
+  }
+}
