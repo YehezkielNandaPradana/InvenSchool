ec02e4d971474fb186b8e87856e59898208804eb
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add dashboard view

---
+++ Mobile/lib/features/dashboard/view/dashboard_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/dashboard/dashboard_page.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const DashboardPage();
+  }
+}
