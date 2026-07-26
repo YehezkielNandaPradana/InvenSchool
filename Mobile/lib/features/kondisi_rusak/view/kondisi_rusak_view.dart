ea5e4bb7def8d37070b7e712d3e661664286c0d6
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 09:55:54 +0700

    add kondisi rusak view

---
+++ Mobile/lib/features/kondisi_rusak/view/kondisi_rusak_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:inventaris_app/features/kondisi_rusak/kondisi_rusak_page.dart';
+
+class KondisiRusakView extends StatelessWidget {
+  const KondisiRusakView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const KondisiRusakPage();
+  }
+}
