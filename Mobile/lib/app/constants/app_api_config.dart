9cf083af8071b1f53a5fa97ec16365c37ae6b47c
Naxx <yehezkieldaniela@gmail.com>
2026-07-26 10:12:38 +0700

    reduce api timeout to 15s

--- Mobile/lib/app/constants/app_api_config.dart
+++ Mobile/lib/app/constants/app_api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   static const String baseUrl = 'http://10.0.2.2:8000/api';
-  static const int connectTimeout = 30000;
-  static const int receiveTimeout = 30000;
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
   static const String tokenKey = 'token';
   static const String userKey = 'user';
 }
